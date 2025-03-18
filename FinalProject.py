b1cf29d576147a9f7a2ea3d989289798a08c0cfe
Logan Reuter <lreuter2020@gmail.com>
2025-03-17 21:27:35 -0700

    commented & final code

--- FinalProject.py
+++ FinalProject.py
@@ -12,6 +12,7 @@ L = 0.47/2 # meters
 PINV_THRESHOLD = 1e-2
 
 def NextState(curr_state: np.ndarray, velocities: np.ndarray, dt: float, max_vel: float) -> np.ndarray:
+    # initialize next state to all zeros
     next_state = np.zeros(13)
 
     velocities[np.where(velocities > max_vel)] = max_vel
@@ -112,16 +113,21 @@ def TrajectoryGenerator(T_se_i, T_sc_i, T_sc_f, T_ce_grasp, T_ce_standoff, k: in
         output[idx] = (t, 0)
         idx += 1
 
+    # used for debugging:
+    # write the calculated trajectory to a csv file
+    # for scene 8
     if write_debug:
         with open("trajectory.csv", "w+") as f:
             writer = csv.writer(f)
             for traj, gripper_state in output:
-                writer.writerow(np.concatenate((traj[:3, :3].flatten(), traj[:3, 3], [gripper_state])))
-        
+                writer.writerow(np.concatenate((traj[:3, :3].flatten(), traj[:3, 3], [gripper_state])))   
 
     return output
 
 def FeedbackControl(joint_angles, X_d, X_d_next, K_p, K_i, delt, joint_constraints: list[bool]):
+    # NOTE: @ in python is used for matrix multiplication
+    
+    # initialize variables used for calculations
     l = L
     w = W
     r = R
@@ -131,23 +137,27 @@ def FeedbackControl(joint_angles, X_d, X_d_next, K_p, K_i, delt, joint_constrain
     x = joint_angles[1]
     y = joint_angles[2]
     
+    # calculate the pseudo inverse of H(0)
     F = r/4 * np.asarray([[-1/(l+w), 1/(l+w), 1/(l+w), -1/(l+w)], [1, 1, 1, 1], [-1, 1, -1, 1]])
     
     F6 = np.zeros((6,4))
     F6[2:5, :] = F
-    
+
     M = np.asarray([[1, 0, 0, 0.033], [0, 1, 0, 0], [0, 0, 1, 0.6546], [0, 0, 0, 1]])
 
     T_b0 = np.asarray([[1, 0, 0, 0.1662], [0, 1, 0, 0], [0, 0, 1, 0.0026], [0, 0, 0, 1]])
     
     T_sb = np.asarray([[np.cos(phi), -np.sin(phi), 0, x], [np.sin(phi), np.cos(phi), 0, y], [0, 0, 1, 0.0963], [0, 0, 0, 1]])
     
+    # calculate the desired twist
     Vd = mr.se3ToVec(1/delt * mr.MatrixLog6(mr.TransInv(X_d) @ X_d_next)).reshape((6,1))
     
+    # Define the body defined screw matrix
     Blist = np.asarray([[0, 0, 1, 0, 0.033, 0], [0, -1, 0, -0.5076, 0, 0], 
                         [0, -1, 0, -0.3526, 0, 0], [0, -1, 0, -0.2176, 0, 0], 
                        [0, 0, 1, 0, 0, 0]]).T
     
+    # Calculate the jacobian for the arm
     Jarm = mr.JacobianBody(Blist, joint_angles[3:])
     
     T_0e = mr.FKinBody(M, Blist, joint_angles[3:])
@@ -157,25 +167,24 @@ def FeedbackControl(joint_angles, X_d, X_d_next, K_p, K_i, delt, joint_constrain
 
     ad2 = mr.Adjoint(mr.TransInv(T_0e)@mr.TransInv(T_b0))
     
+    # calculate the jacobian for the base
     Jbase = ad2 @ F6
     
+    # calculate the whole jacobian
     J = np.concatenate((Jbase,Jarm), axis=1)
-    # J[:, joint_constraints] = 0
 
-    Xerr = mr.se3ToVec(mr.MatrixLog6(mr.TransInv(X) @ X_d)).reshape((6,1))
-    ad = mr.Adjoint(mr.TransInv(X) @ X_d)
-    error += Xerr*delt
-
-    V = ad @ Vd + K_p @ Xerr + K_i @ error
-    
-    output = np.linalg.pinv(J, rcond=PINV_THRESHOLD) @ V
-
+    # calculate the A matrix for manipulability calculations
     Aw = J[:3, :] @ J[:3, :].T
     Av = J[3:, :] @ J[3:, :].T
 
+    # find the eigenvalues of the A matrices
     eig_w = np.linalg.eigvals(Aw)
     eig_v = np.linalg.eigvals(Av)
 
+    # calculate the manipulability
+    # handling division by 0 errors by
+    # treating all numbers less than PINV_THRESHOLD
+    # as zero
     manip_w = -1
     manip_v = -1
     if abs(min(eig_w)) > PINV_THRESHOLD:
@@ -183,6 +192,20 @@ def FeedbackControl(joint_angles, X_d, X_d_next, K_p, K_i, delt, joint_constrain
     
     if abs(min(eig_v)) > PINV_THRESHOLD:
         manip_v = math.sqrt( max(eig_v) / min(eig_v) )
+
+    # used to update jacobian for singularity and joint limits
+    # J[:, joint_constraints] = 0
+
+    # calculate the error
+    Xerr = mr.se3ToVec(mr.MatrixLog6(mr.TransInv(X) @ X_d)).reshape((6,1))
+    ad = mr.Adjoint(mr.TransInv(X) @ X_d)
+    error += Xerr*delt
+
+    # use feedforward + feedback control to calculate twist
+    V = ad @ Vd + K_p @ Xerr + K_i @ error
+    
+    # calculate the new joint velocities
+    output = np.linalg.pinv(J, rcond=PINV_THRESHOLD) @ V
 
     return (output, Xerr, manip_w, manip_v)
 
@@ -192,16 +215,16 @@ def test_state(curr_state: np.ndarray) -> list[bool]:
     arm_joints = curr_state[3:8]
     arm_constr_offst = 4
 
-    # if arm_joints[0] < -1e-2 or arm_joints[0] > 1e-2:
-    #     joint_constraints[4] = True
+    # check for joint limits and positions that could lead to
+    # singularities
 
-    if arm_joints[1] > np.pi/2 or arm_joints[1] < -1.1:
+    if arm_joints[1] > np.pi/2 or arm_joints[1] < -1:
         joint_constraints[5] = True
 
-    if arm_joints[2] > -0.1:
-        joint_constraints[4] = True
+    if arm_joints[2] > -0.2 and arm_joints[2] < -2:
+        joint_constraints[6] = True
 
-    if arm_joints[3] > -0.1:
+    if arm_joints[3] > -0.2 and arm_joints[3] < -1.7:
         joint_constraints[7] = True
 
     return joint_constraints
@@ -215,110 +238,116 @@ def FullProgram() -> None:
         K_p: float Proportional gain
         K_i: float Integral Gain
     """
-
+    # initialize lists to store useful information
     steps = []
     err_v_time = []
     linear_manip = []
     angular_manip = []
 
-    # T_se_i = np.array([[0.170, 0, 0.985, 0.387], [0, 1, 0, 0], [-0.985, 0, 0.170, 0.570], [0, 0, 0, 1]])
-    
-    # Original Task Config
-    T_se_i = np.array([[0, 0, 1, 0], [0, 1, 0, 0], [-1, 0, 0, 0.5], [0, 0, 0, 1]])
-    T_sc_i = np.array([[1, 0, 0, 1], [0, 1, 0, 0], [0, 0, 1, 0.25], [0, 0, 0, 1]])
-    T_sc_f = np.array([[0, 1, 0, 0], [-1, 0, 0, -1], [0, 0, 1, .25], [0, 0, 0, 1]])
+    # T_se_i = np.array([[0, 0, 1, 0], [0, 1, 0, 0], [-1, 0, 0, 0.5], [0, 0, 0, 1]])
+    T_se_i = np.array([[-1, 0, 0.002, 0.335], [0, 1, 0, 0], [-0.002, 0, -1, 0.183], [0, 0, 0, 1]])
     T_ce_grasp = np.array([[-1, 0, 0, 0], [0, 1, 0, 0], [0, 0, -1, -0.25], [0, 0, 0, 1]])
     T_ce_standoff = np.array([[-1, 0, 0, 0], [0, 1, 0, 0], [0, 0, -1, -0.15], [0, 0, 0, 1]])
+    
+    # Original Task Config
+    T_sc_i = np.array([[1, 0, 0, 1], [0, 1, 0, 0], [0, 0, 1, 0.25], [0, 0, 0, 1]])
+    T_sc_f = np.array([[0, 1, 0, 0], [-1, 0, 0, -1], [0, 0, 1, .25], [0, 0, 0, 1]])
 
     # New Task Config
-    # T_se_i = np.array([[-1, 0, 0.002, 0.335], [0, 1, 0, 0], [-0.002, 0, -1, 0.183], [0, 0, 0, 1]])
     # T_sc_i = np.array([[1, 0, 0, 1.5], [0, 1, 0, 0.5], [0, 0, 1, 0.25], [0, 0, 0, 1]])
     # T_sc_f = np.array([[1, 0, 0, 1.5], [0, 1, 0, -0.5], [0, 0, 1, 0.25], [0, 0, 0, 1]])
-    # T_ce_grasp = np.array([[-1, 0, 0, 0], [0, 1, 0, 0], [0, 0, -1, -0.25], [0, 0, 0, 1]])
-    # T_ce_standoff = np.array([[-1, 0, 0, 0], [0, 1, 0, 0], [0, 0, -1, -0.15], [0, 0, 0, 1]])
 
     # Best:
-    K_p = np.eye(6) * [1, 0.15, 1, 0.7, 1, 0.25]
-    K_i = np.zeros((6,6))
+    K_p = np.eye(6) * [1, 0.15, 1, 0.15, 1, 1]
+    K_i = np.eye(6) * 50
 
-    # Overshoot:
+    # Overshoot (Result 1):
+    # K_p = np.eye(6) * [1, 0.15, 1, 0.85, 1, 1]
+    # K_i = np.eye(6) * 50
+
+    # Overshoot (Result 2):
     # K_p = np.eye(6) * [1, 0.15, 1, 5, 1, 0.25]
-    # K_i = np.eye(6) * [20, 20, 20, 20, 20, 20]
+    # K_i = np.eye(6) * 50
 
     # New Task:
-    # K_p = np.eye(6) * [1, 0.5, 1, 0.7, 1, 0.75]
+    # K_p = np.eye(6) * [1, 0.35, 1, 0.25, 1, 0.5]
     # K_i = np.zeros((6,6))
 
-    k = 1 # number of steps per 0.001 seconds
+    k = 1 # number of steps per 0.01 seconds
     Tf = 2 # trajectory runtime (s)
-    dt = 0.01
+    dt = 0.01 # time step (s)
     max_speed = 5 # m/s
 
     trajectory = TrajectoryGenerator(T_se_i, T_sc_i, T_sc_f, T_ce_grasp, T_ce_standoff, k, Tf, write_debug=True)
     curr_state = np.array([0, 0, 0, 0, 0, -np.pi/4, -np.pi/4, 0, 0, 0, 0, 0, 0])
 
+    # list to store all determined singularities for debugging purposes
+    singularities = []
+
+    # loop through each step in the trajector
     for idx, traj in enumerate(trajectory):
+        # get the current reference configuration and gripper state
         step, gripper_state = traj
         if idx == len(trajectory) - 1:
             break
         
-        T_se_d = step
-        T_se_d_next, _ = trajectory[idx + 1]
+        T_se_d = step # assign reference frame
+        T_se_d_next, _ = trajectory[idx + 1] # retrieve next reference frame
 
-        joint_constraints = [False for _ in range(9)]
+        # check for singularities and joint limits
+        joint_constraints = test_state(curr_state)
 
+        # calculate the require joint velocities, error, and manipulability
         velocities, err, manip_w, manip_v = FeedbackControl(np.array(curr_state[:8]), T_se_d, T_se_d_next, K_p, K_i, dt, joint_constraints)
 
-        next_state = NextState(curr_state, velocities.reshape(1, 9)[0], dt, max_speed)
+        # determine the next state based on the joint velocities
+        curr_state = NextState(curr_state, velocities.reshape(1, 9)[0], dt, max_speed)           
 
-        joint_constraints = test_state(next_state)
-
-        if True in joint_constraints:
-            velocities, err, manip_w, manip_v = FeedbackControl(np.array(curr_state[:8]), T_se_d, T_se_d_next, K_p, K_i, dt, joint_constraints)
-
-            next_state = NextState(curr_state, velocities.reshape(1, 9)[0], dt, max_speed)            
-
-        curr_state = next_state
-
+        # add gripper state to the end of the current state
         curr_state[12] = gripper_state
 
+        # append all useful data
         steps.append(curr_state)
         err_v_time.append(err)
         angular_manip.append(manip_w)
         linear_manip.append(manip_v)
+
+        # append any singularities for debugging
+        if manip_w == -1:
+            singularities.append(curr_state)
     
+    # write all configuration data to a csv file for simulation
     with open("final.csv", "w+") as file:
         writer = csv.writer(file)
 
         writer.writerows(steps)
     
+    # print the first singularity point
+    if singularities:
+        print(singularities[0])
+
+    # plot the error over time and the manipulability as a function of time
     err_v_time = np.array(err_v_time)
+    t = np.linspace(0, err_v_time.shape[0], err_v_time.shape[0]) * dt
+    for idx in range(len(err_v_time[0])):
+        plt.plot(t, err_v_time[:, idx], label=f"{idx + 1}")
+    plt.legend()
+    plt.xlabel("Time (s)")
+    plt.ylabel("Error")
+    plt.savefig(f"./tmp/overshoot.png")
+
+    plt.clf()
+    
     angular_manip = np.array(angular_manip)
     angular_manip[np.where(angular_manip == -1)] = max(angular_manip)
     linear_manip = np.array(linear_manip)
     linear_manip[np.where(linear_manip == -1)] = max(linear_manip)
-    t = np.linspace(0, err_v_time.shape[0], err_v_time.shape[0]) * dt
-    # for idx in range(len(err_v_time[0])):
-    #     plt.plot(t, err_v_time[:, idx], label=f"{idx + 1}")
     plt.plot(t, angular_manip, label="u_1(Aw)")
     plt.plot(t, linear_manip, label="u_1(Av)")
     plt.legend()
+    plt.xlabel("Time (s)")
+    plt.ylabel("Manipulability")
     plt.show()
     
-
+# run the full program
 FullProgram()
-
-# X = np.array([[0.170, 0, 0.985, 0.387], [0, 1, 0, 0], [-0.985, 0, 0.170, 0.570], [0, 0, 0, 1]])
-# Xd = np.array([[0, 0, 1, 0.5], [0, 1, 0, 0], [-1, 0, 0, 0.5], [0, 0, 0, 1]])
-# Xd_next = np.array([[0, 0, 1, 0.6], [0, 1, 0, 0], [-1, 0, 0, 0.3], [0, 0, 0, 1]])
-# curr_state = np.array([0, 0, 0, 0, 0, 0.2, -1.6, 0, 0, 0, 0, 0, 0])
-# dt = 0.01
-# K_p = np.zeros((6,6))
-# K_i = np.zeros((6,6))
-# vel, err, T_se = FeedbackControl(X, Xd, Xd_next, K_p, K_i, dt, curr_state[:8])
-
-# next_st = NextState(curr_state, vel.reshape(1, 9)[0], dt, 10)
-# with open("test.csv", "w+") as f:
-#     writer = csv.writer(f)
-#     writer.writerow(curr_state)
-#     writer.writerow(next_st)
